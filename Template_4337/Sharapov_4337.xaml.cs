44db4ac748b80ab6433dbb9762e5857982531458
kwr1337 <89042002+kwr1337@users.noreply.github.com>
2023-04-29 01:04:05 +0300

    Добавление кнопки, обработчика событий и окна

--- Template_4337/Sharapov_4337.xaml.cs
+++ Template_4337/Sharapov_4337.xaml.cs
@@ -10,30 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Template_4337
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Sharapov_4337.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Sharapov_4337 : Window
     {
-        public MainWindow()
+        public Sharapov_4337()
         {
             InitializeComponent();
-        }
-        
-        private void Khuzyakaev_4337_Click(object sender, RoutedEventArgs e)
-        {
-            var window = new Khuzyakaev_4337();
-            window.Show();
-        }
-
-        private void Sharapov_4337_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
